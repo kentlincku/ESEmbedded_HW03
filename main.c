6df467ab367e4add38f717046a997ed5a8fdd4cd
Your Name <111>
2019-03-19 21:10:19 +0800

    renew

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-int summand=9;
 int add(int a, int b){return a+b;}
+int addbad(int a, int b, int c){return c;}
 void addp(int *a, int *b, int *c){*c=*a+*b;}
 void reset_handler(void)
 {
+	int summand=9;
 	int addend=10;
 	int sum=0;
 	sum=add(summand, addend);
+	sum=0;
+	sum=addbad(summand, addend, sum);
 	sum=0;
 	addp(&summand, &addend, &sum);
 	while (1);
